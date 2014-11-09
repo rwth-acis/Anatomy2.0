7e9a0f0e93f9196020262e575c975e800d5f99ed
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2014-11-09 12:49:39 +0100

    overview-prototype: table for database entries of models

--- Repository/index.php
+++ Repository/index.php
@@ -1,35 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head>
- <title> 3D Models</title>
- <meta charset='utf-8'/>
- <meta name='description' content='TODO'/>
- <script type='text/javascript' src='http://www.x3dom.org/download/x3dom.js'> </script>
- <link rel='stylesheet' type='text/css' href='http://www.x3dom.org/download/x3dom.css'></link> 
-</head>
-<body>
- <?php
-  $verbindung = mysql_connect("localhost", "root","")
-   or die ("keine Verbindung moeglich. 
-   Benutzername oder Passwort sind falsch");
-  mysql_select_db("3dmodelsdb") 
-   or die ("Die Datenbank existiert nicht.");
- ?>
+  <head>
+    <title> 3D Models</title>
+    <meta charset='utf-8'/>
+    <meta name='description' content='TODO'/>
+    <script type='text/javascript' src='http://www.x3dom.org/download/x3dom.js'> </script>
+    <link rel='stylesheet' type='text/css' href='http://www.x3dom.org/download/x3dom.css'></link> 
+  </head>
+  <body>
+    <!-- connect to database -->
+    <?php
+       $verbindung = mysql_connect("localhost", "root","")
+       or die ("keine Verbindung moeglich. 
+		Benutzername oder Passwort sind falsch");
+       mysql_select_db("3dmodelsdb") 
+       or die ("Die Datenbank existiert nicht.");
+       ?>
 
-<h1>3D Models</h1>
-<?php
- $abfrage = "SELECT url, urlname FROM model";
- $ergebnis = mysql_query($abfrage);
- while($row = mysql_fetch_object($ergebnis))
- {
-  echo "$row->urlname: $row->url <br>";
-  echo "<x3d width='600px' height='400px'>
- <scene>
-<inline url=\"$row->url\" > </inline>
- </scene> 
-</x3d>";
- }
-?>
+    <h1>Collaborative 3D Model Viewing</h1>
+    <h2>Select a model:</h2>
+    <table border="2" cellpadding="10">
+      <tr>
+	<th>Model Name</th>
+	<th>Preview</th>
+      </tr>
+      <!-- Get entries from the database and create a table row for each one -->
+      <?php
+	 $abfrage = "SELECT url, urlname FROM model";
+	 $ergebnis = mysql_query($abfrage);
+	 while($row = mysql_fetch_object($ergebnis))
+	 {
+	 echo "<tr><td>$row->urlname</td>";
+	 echo "<td><x3d width='100px' height='100px'>
+	       <scene>
+		 <inline url=\"models/$row->url\" > </inline>
+	       </scene> 
+               </x3d></td></tr>";
+      }
+      ?>
+    </table>
 
-</body>
+  </body>
 </html>
