e10e49ca3d8d04376c354f3894a3563330980c29
Dev Sharma <devsharma1210@gmail.com>
2014-11-22 18:53:41 +0100

    Finally working on ROLE SDK, @ Alex you can now make changes to this branch.

--- src/js/publisher.js
+++ src/js/publisher.js
@@ -1,61 +1,60 @@
 var pos;
-var rot;
+        var rot;
 
-// IWC initialize section.
-var iwcClient;
+        // IWC initialize section.
+        var iwcClient;
+        gadgets.util.registerOnLoadHandler(init);
+        function init () {
+            iwcClient = new iwc.Client();
+            iwcClient.connect(iwcCallback);
+        }
 
-function init (callback) {
-    var iwcCallback = callback ? callback : function(){};       // if no function is defined, define empty function
-    iwcClient = new iwc.Client();
-    iwcClient.connect(iwcCallback);
-}
+        // Viewport section.
+        document.onload = function()
+        {
+            document.getElementById('viewport').addEventListener('viewpointChanged', viewpointChanged, false);
+        }
+        document.body.onmouseup = function() {
+            // Send through IWC!
+            var intent = {
+                "component" :"",                            // recipient, empty for broadcast
+                "data"      :"http://data.org/some/data",   // data as URI
+                "dataType"  :"text/xml",                    // data mime type
+                "action"    :"ACTION_UPDATE",               // action to be performed by receivers
+                "flags"     :["PUBLISH_GLOBAL"],            // control flags
+                "extras"    :{"position": pos, "orientation": rot} // optional auxiliary data
+            }
+            if(iwc.util.validateIntent(intent)) {
+                iwcClient.publish(intent);
 
-// Viewport section.
-function sendPositionAndOrientation() 
-{
-    // Send through IWC!
-    var intent = {
-        "component" :"",                            // recipient, empty for broadcast
-        "data"      :"http://data.org/some/data",   // data as URI
-        "dataType"  :"text/xml",                    // data mime type
-        "action"    :"ACTION_UPDATE",               // action to be performed by receivers
-        "flags"     :["PUBLISH_GLOBAL"],            // control flags
-        "extras"    :{"position": pos, "orientation": rot} // optional auxiliary data
-    }
-    if(iwc.util.validateIntent(intent)) {
-        iwcClient.publish(intent);
+                var camPos = pos.x.toFixed(4) + ' ' +
+                        pos.y.toFixed(4) + ' ' +
+                        pos.z.toFixed(4);
+                var camRot = rot[0].x.toFixed(4) + ' ' +
+                        rot[0].y.toFixed(4) + ' ' +
+                        rot[0].z.toFixed(4) + ' ' +
+                        rot[1].toFixed(4);
+                document.getElementById("debugText").innerHTML = "Sent viewpoint position = " +
+                        camPos + " orientation = " + camRot;
+            }
+            else {
+                alert("Intent not valid! ");
+            }
+        }
+        function viewpointChanged(evt)
+        {
+            if (evt) {
+                pos = evt.position;
+                rot = evt.orientation;
 
-        var camPos = pos.x.toFixed(4) + ' ' +
-                pos.y.toFixed(4) + ' ' +
-                pos.z.toFixed(4);
-        var camRot = rot[0].x.toFixed(4) + ' ' +
-                rot[0].y.toFixed(4) + ' ' +
-                rot[0].z.toFixed(4) + ' ' +
-                rot[1].toFixed(4);
-        document.getElementById("debugText").innerHTML = "Sent viewpoint position = " +
-                camPos + " orientation = " + camRot;
-    }
-    else {
-        alert("Intent not valid! ");
-    }
-}
-
-function viewpointChanged(evt)
-{
-    if (evt) {
-        pos = evt.position;
-        rot = evt.orientation;
-
-        var camPos = pos.x.toFixed(4) + ' ' +
-                pos.y.toFixed(4) + ' ' +
-                pos.z.toFixed(4);
-        var camRot = rot[0].x.toFixed(4) + ' ' +
-                rot[0].y.toFixed(4) + ' ' +
-                rot[0].z.toFixed(4) + ' ' +
-                rot[1].toFixed(4);
-        document.getElementById("debugText").innerHTML = "Updated viewpoint position = " +
-                camPos + " orientation = " + camRot;
-
-        sendPositionAndOrientation
-    }
-}
+                var camPos = pos.x.toFixed(4) + ' ' +
+                        pos.y.toFixed(4) + ' ' +
+                        pos.z.toFixed(4);
+                var camRot = rot[0].x.toFixed(4) + ' ' +
+                        rot[0].y.toFixed(4) + ' ' +
+                        rot[0].z.toFixed(4) + ' ' +
+                        rot[1].toFixed(4);
+                document.getElementById("debugText").innerHTML = "Updated viewpoint position = " +
+                        camPos + " orientation = " + camRot;
+            }
+        }
