b560e00e49cbe4e0a7f3a36ee89a7f9e95328f34
Alexandra Wörner <alexandra.woerner@rwth-aachen.de>
2014-12-14 15:15:21 +0100

    Close #19: Fix jasmine test suites Added test suites for wrapper and subsite, added some tests for the viewer. Moved some variable declariations to top of corresponding files for better readability and accessibility.

---
+++ src/tests/jasmine/spec/Init-WrapperSpec.js
@@ -0,0 +1,113 @@
+/**
+ * Tests from init-wrapper.js:
+ */
+describe('The wrapper', function() {
+  
+  var intent;
+  beforeEach(function() {
+    intent = getDefaultIntent();
+  });
+
+  beforeAll(function() {
+    iwcClient = new iwc.Client();
+    contentWindow = window;
+  });
+
+  afterAll(function() {
+    iwcClient = null;
+    contentWindow = null;
+  });
+
+  it('has and id of length 9', function() {
+	expect(id).toBeDefined();
+	expect(id.length).toEqual(9);
+  });
+
+  // Check correct behaviour of iwcCallback
+  it('does not update the viewpoint if the received message comes from itself', 
+      function() {
+        spyOn(window, 'receivedViewpointChanged');
+
+        iwcCallback(intent);
+        expect(receivedViewpointChanged).not.toHaveBeenCalled();
+  });
+
+  it('handles messages with an undefined or unknown topic', function() {
+    var tmp = widget;
+    widget = 'widget';
+    spyOn(console, 'log');
+
+    iwcCallback(intent);
+    intent.extras.topic = 'SubsiteLoaded';
+    iwcCallback(intent);
+    expect(console.log.calls.count()).toEqual(2);
+
+    widget = tmp;
+  });
+
+  // iwcCallback and receivedViewpointChanged
+  it('passes the received viewpoint to the iframe', function() {
+    var contentWindow = window;
+    var tmp = widget;
+    widget = 'widget';
+    intent.extras = {
+      topic: 'ViewpointUpdate', 
+      position: {x:0, y:0, z:0},
+      orientation: [{x:0,y:0,z:0},0]
+    };
+    spyOn(contentWindow, 'postMessage');
+
+    iwcCallback(intent);
+    expect(contentWindow.postMessage).toHaveBeenCalledWith('ViewpointUpdate ' +
+        '{"topic":"ViewpointUpdate","position":{"x":0,"y":0,"z":0},"' +
+        'orientation":[{"x":0,"y":0,"z":0},0]}', '*');
+    
+    widget = tmp;
+  });
+
+  // publishMessage
+  it('warns the user if the message to be published is invalid', function() {
+    var intent = {};
+    spyOn(iwc.util, 'validateIntent');
+    spyOn(window, 'alert');
+
+    publishMessage(intent);
+    expect(alert).toHaveBeenCalled();
+  });
+
+  it('publishes a valid message', function() {
+    spyOn(iwcClient, 'publish');
+
+    publishMessage(intent);
+    expect(iwcClient.publish).toHaveBeenCalled();
+  });
+
+  // getDefaultIntent
+  it('creates a valid default intent', function() {
+    var tmp = widget;
+    widget = 'widget';
+    var intent = getDefaultIntent();
+
+    expect(iwc.util.validateIntent(intent)).toBeTruthy();
+    expect(intent.sender).toBe(widget);
+
+    widget = tmp;
+  });
+
+  //receiveSubsiteMessage
+
+  //publishViewpointChanged
+
+});
+
+describe('The wrapper when gadgets is defined', function() {     
+  var gadgets = gadgets || undefined;     
+
+  if(gadgets !== undefined) {
+        
+      it('is added to the communication channel', function(){
+        expect(iwcClient).toBeDefined();
+      });
+
+  }
+});
