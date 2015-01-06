08b29b3129121bcae60de25e742d96872dc0089b
Dev Sharma <devsharma1210@gmail.com>
2015-01-06 17:13:26 +0100

    Removed the footer!

--- src/views/model_viewer.php
+++ src/views/model_viewer.php
@@ -105,7 +105,6 @@
       ?>
     </div>
   </div>  
-    <?php include("footer.html"); ?>
     
     <!-- Functionality of menu toolbar -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
