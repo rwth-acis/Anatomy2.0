f584e4c2fa2dbb6b63b4b879a5ee57f1bf162611
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2015-01-20 18:06:38 +0100

    Courses: added first creation page for courses
    Currently the creating user is hardcoded!
    Page is mainly a modified version of the model upload page.

---
+++ src/php/upload_script_course.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+* @file upload_script_course.php
+* 
+* Adss new course to the course database on the server
+* adds metadata about it database.
+*/
+//Get input data from form
+$name = $_POST['name'];
+$text = $_POST['text'];
+$role_link = $_POST['roleLink'];
+$preview_img_link = $_POST['previewImgLink'];
+
+//TODO: get id of currently logged in user
+$creator=17;
+	
+// Create database-entry
+$conn = require '../php/db_connect.php';
+
+$sql = "INSERT INTO courses (name, description, creator, role_url, img_url) VALUES ('$name','$text', $creator, '$role_link', '$preview_img_link')";
+
+echo "sqlquery: $sql";
+
+$conn->query($sql);
+
+$last_id = $conn->lastInsertId();
+
+header("Location: ../views/success-course-upload.php?id=$last_id");
+
+
+echo 'Successfully added course';
+	
---
+++ src/views/success-course-upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Create a new course</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf8"/>
+     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  </head>
+  <body>
+	<div class="container">
+  		<div class="alert alert-success" role="alert">Your Course has been added successfully</div>
+	<a  href="course.php?id=<?php echo $_GET['id']?>" >See your Course Page</a>
+	</div>
+  </body>
+
+</html>
