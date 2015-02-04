765427149669227dc32937c81874cab0ac1372fb
Alexandra Wörner <alexandra.woerner@rwth-aachen.de>
2015-02-05 00:24:06 +0100

    Model list: Fix wrong call to size attribute of model

--- src/php/tools.php
+++ src/php/tools.php
@@ -32,31 +32,38 @@ function createTable($result, $type) {
  * @return string/html        HTML containing the model information
  */
 function getModelStructure($entry, $type) {
-
+    $formatBytes = function ($bytes, $precision = 2) { 
+        $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+  
+        $bytes = max($bytes, 0); 
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+        $pow = min($pow, count($units) - 1); 
+        $bytes /= pow(1024, $pow);
+  
+        return round($bytes, $precision) . ' ' . $units[$pow]; 
+    };
+    
     $html = "";
     switch ($type) {
         case 'model':
-            $html .= 
-          "<li>
-              <a href='model_viewer.php?id=$entry[id]' id='a_img$entry[id]'><img id='image-over$entry[id]' name='image-over' src='../../$entry[preview_url]' alt=$entry[name] width='150' height='150' />
-              <span class='text-content'><span>Name: $entry[name]<br>Size: $entry[size]<br> Category: $entry[classification]</span></span></a>
+            $html .= "<li><a href='model_viewer.php?id=$entry[id]' id='a_img$entry[id]'><img id='image-over$entry[id]' name='image-over' src='../../$entry[preview_url]' alt=$entry[name] width='150' height='150' />
+              <span class='text-content'><span>Name: $entry[name]<br>Size: ".$formatBytes($entry["size"])."<br> Category: $entry[classification]</span></span></a>
               <p id='text-over'>$entry[name]</p>
               </li>";
-	    break;
+	          break;
         
         case 'modelselection':
             $html .= "<li><img id='image-over$entry[id]' src='../../$entry[preview_url]' alt=$entry[name] name='image-over' width='150' height='150' style='margin-top:5px;' />
-              <span class='text-content'><span>Name: $entry[name]<br>Size: $entry[size]<br> Category: $entry[classification]</span></span>
+              <span class='text-content'><span>Name: $entry[name]<br>Size: ".$formatBytes($entry["size"])."<br> Category: $entry[classification]</span></span>
               <p id='text-over' style='margin-left:5px;'>$entry[name]</p>
               </li>";
             break;
 
         default:
-            $html .= 
-          "<li><a href='model_viewer.php?id=$entry[id]' id='a_img$entry[id]'><img id='image-over$entry[id]' name='image-over' src='../../$entry[preview_url]' alt=$entry[name] width='150' height='150' />
-              <span class='text-content'><span>Name: $entry[name]<br>Size: $entry[size]<br> Category: $entry[classification]</span></span></a>
+            $html .= "<li><a href='model_viewer.php?id=$entry[id]' id='a_img$entry[id]'><img id='image-over$entry[id]' name='image-over' src='../../$entry[preview_url]' alt=$entry[name] width='150' height='150' />
+              <span class='text-content'><span>Name: $entry[name]<br>Size: ".$formatBytes($entry["size"])."<br> Category: $entry[classification]</span></span></a>
               <p id='text-over'>$entry[name]</p>
-	      <div class='delete' id='$entry[id]'></div>
+	            <div class='delete' id='$entry[id]'></div>
               </li>";
             break;
     }
