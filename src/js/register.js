45ff51468fc739d7e7c2c656893a79140152f7ac
dost25 <dominik.studer@rwth-aachen.de>
2015-02-05 17:46:40 +0100

    Register: Validation of inputs is properly done

--- src/js/register.js
+++ src/js/register.js
@@ -23,22 +23,24 @@ function onRegisterClicked() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
   
-  // Show a little animated "loading" image
-  document.getElementById('register_loader').style.display = "inline";
-  // Send a post request to server to create a new user in database
-  ajax.post("../php/register.php", {register_email:email, register_pw:password}, function(msg) {
-    msg = JSON.parse(msg);
-    // Redirect to "success" page if account creation was successfull
-    if (msg.result === "ok") {
-      window.location.href = "register_success.php";
-    }
-    // Otherwise show an error message. At the moment the only known error is, that the account already exists.
-    else {
-      document.getElementById('error').innerHTML = "An account for this email address already exists.";
-    }
-    // Hide "loading" animation
-    document.getElementById('register_loader').style.display = "none";    
-  });
+  if ($('#form_login').parsley().isValid()) {
+    // Show a little animated "loading" image
+    document.getElementById('register_loader').style.display = "inline";
+    // Send a post request to server to create a new user in database
+    ajax.post("../php/register.php", {register_email:email, register_pw:password}, function(msg) {
+      msg = JSON.parse(msg);
+      // Redirect to "success" page if account creation was successfull
+      if (msg.result === "ok") {
+        window.location.href = "register_success.php";
+      }
+      // Otherwise show an error message. At the moment the only known error is, that the account already exists.
+      else {
+        document.getElementById('error').innerHTML = "An account for this email address already exists.";
+      }
+      // Hide "loading" animation
+      document.getElementById('register_loader').style.display = "none";    
+    });
+  }
 }
 
 /// Call initialize for registration when DOM loaded
