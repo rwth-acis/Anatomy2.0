59c4598015fe72107c8bd14047e303d6436a8149
demiralp <demiralpali@gmail.com>
2015-02-05 04:18:12 +0100

    [Overview]: Closing the toolbar once again works.

--- src/views/menu.php
+++ src/views/menu.php
@@ -12,6 +12,8 @@
     <script src="../js/menu_logged_out.js" type="text/javascript"></script>
   <?php } ?>
 
+  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
   <div class='navbar navbar-inverse'>
     <div class='container'>
       <div class='navbar-header'>
@@ -21,14 +23,19 @@
   if(isset($_GET["widget"]) && $_GET["widget"] == "true")
   {
     //we have to link to the widget versions:
-    print("<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
+    print("
+        <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#henm-nav-bar'>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+        </button>
         <a class='navbar-brand' href='welcome.php?widget=true'>
           <img src='../images/logo.png' height='30' alt='HENM: 3D Models'>
         </a>
       </div>
-      <div class='navbar-collapse collapse'>
+      <div id='henm-nav-bar' class='collapse navbar-collapse'>
         <ul class='nav navbar-nav pull-right mainNav'>
-	  <li><a href='welcome.php?widget=true'>Home</a></li>
+	        <li><a href='welcome.php?widget=true'>Home</a></li>
           <li><a href='courses.php?widget=true'>Courses</a></li>
           <li><a href='overview.php?widget=true'>Models</a></li>
           <li><a href='upload.php?widget=true'>Upload</a></li>
@@ -37,14 +44,19 @@
   else
   {
     //we have to link to the non-widget versions:
-    print("<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
+    print("
+        <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#henm-nav-bar'>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+        </button>
         <a class='navbar-brand' href='welcome.php'>
           <img src='../images/logo.png' alt='HENM: 3D Models'>
         </a>
       </div>
-      <div class='navbar-collapse collapse'>
+      <div id='henm-nav-bar' class='collapse navbar-collapse'>
         <ul class='nav navbar-nav pull-right mainNav'>
-	  <li><a href='welcome.php'>Home</a></li>
+	        <li><a href='welcome.php'>Home</a></li>
           <li><a href='courses.php'>Courses</a></li>
           <li><a href='overview.php'>Models</a></li>
           <li><a href='role.php'>ROLE</a></li>
@@ -67,4 +79,3 @@
 
   <!-- Allows opening dropdown menu if screen size is small -->
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
