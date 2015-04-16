4960b37feb129c50dff727349115f7489054627b
dost25 <dominik.studer@rwth-aachen.de>
2015-04-16 12:02:54 +0200

    Configuration: The database configuration can now be done in a separate config file.
    Added documentation on how and where to configure.

---
+++ src/config/database_placeholder.php
@@ -0,0 +1,10 @@
+<?php
+  // URL to the server where the database is hosted
+	$host = "";
+  // The name of the database
+	$database = "";
+  // The database user which can access the database
+	$user = "";
+  // The password for the database user
+	$password = "";
+?>
