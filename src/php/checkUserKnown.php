04b10449ffe6d1b1cfb1457c540406a224421456
adabru <b.brunnmeier@gmx.de>
2015-05-26 17:11:10 +0200

    Userlogin via las2peer

--- src/php/checkUserKnown.php
+++ src/php/checkUserKnown.php
@@ -23,17 +23,25 @@
   $access_token = filter_input(INPUT_POST, 'access_token');
 
   if (isset($access_token)) {
-    if ($openIdConnectEmail !== FALSE && $openIdConnectSub !== FALSE) {
       require '../php/db_connect.php';
-  		require '../php/tools.php'
-  		
-  		TODO httpRequest(...)
-      // Use only when debugging
-      //$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+      require '../config/config.php';
+  		require '../php/tools.php';
 
+  		$aRes = httpRequest("GET", $las2peerUrl.'/'.'user'.'?access_token='.$access_token);
+      // Use only when debugging
+	  	// require '../php/fb.php';
+
+      $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+      
+      if($aRes->bOk == FALSE or $aRes->iStatus !== 200) {
+      	error_log('3dnrt/user call unsuccessfull: '.$aRes.sMsg);
+      	die('Cannot retrieve User-information!');
+      }
+      $userProfile = json_decode($aRes->sMsg);
+      
       // FIRST OF ALL, CHECK WHETHER THE USER IS KNOWN TO THE SYSTEM
       // THIS IS DONE BY CHECKING WHETHER THE UNIQUE OPEN ID CONNECT SUB EXISTS IN OUR DATABASE
-      $sqlSelect = "SELECT * FROM `users` WHERE openIdConnectSub='$openIdConnectSub'";
+      $sqlSelect = "SELECT * FROM `users` WHERE openIdConnectSub='".$userProfile->sub."'";
 
       // This will escape symbols in the SQL statement (also supposed to prevent 
       // SQL injection attacks). Returns a PDOStatement
@@ -45,14 +53,18 @@
       // If $user is empty, the user is not known
       if(!$user) {
         // CREATE A NEW USER DATABASE ENTRY IF USER WAS NOT KNOWN TO THE SYSTEM
+		  $sqlInsert = "INSERT INTO users (email, openIdConnectSub, given_name, family_name) VALUES ('".$userProfile->email."','".$userProfile->sub."','".$userProfile->given_name."','".$userProfile->family_name."')";
         $sth = $db->prepare($sqlInsert);
         // Executes the select statement on DB
         $ret = $sth->execute();
+        if($ret === false) {
+          error_log('Error: user insertion in database failed!');
+          die('Could not create new user.');
+        }
       }
       else {
         $confirmed = $user['confirmed'];
       }
-    }
   }
   
   $result = ['confirmed'=>$confirmed, 'sqlSelect'=>$sqlSelect, 'sqlInsert'=>$sqlInsert];		
