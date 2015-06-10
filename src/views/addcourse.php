ca3477382dcc326a61392d222af4baf1a78a62e6
dost25 <dominik.studer@rwth-aachen.de>
2015-06-10 14:17:34 +0200

    Responsive design: Some pages missed meta tag for responsive design. Now added to all

--- src/views/addcourse.php
+++ src/views/addcourse.php
@@ -23,6 +23,7 @@
   <head>
     <title>Create a new course</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Additional styles -->
     <link rel='stylesheet' type='text/css' href='../css/bootstrap.min.css'>
     <!--<link rel='stylesheet' type='text/css' href='../css/style.css'> -->
--- src/views/upload.php
+++ src/views/upload.php
@@ -23,6 +23,7 @@
   <head>
     <title>Upload Page</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Additional styles -->
     <link rel='stylesheet' type='text/css' href='../css/bootstrap.min.css'>
     <!-- To be removed?
