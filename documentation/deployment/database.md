7a4c6a699a3735c7a7b3235cae8167228a34a12e
adabru <b.brunnmeier@gmx.de>
2016-02-03 16:30:19 +0100

    code collapse :)

--- documentation/deployment/database.md
+++
@@ -1,8 +0,0 @@
-# Database setup
-
-Note: As a tool to create a MySQL database and configure its tables, you may use phpmyadmin. It comes with XAMPP installation. http://www.danielmois.com/article/Manage_remote_databases_from_localhost_with_phpMyAdmin has a tutorial on how to use phpMyAdmin from XAMPP to do database management.
-
-To set up a database for Anatomy2.0, execute the following two steps:
-
-1. Create a new database (choose whatever name you think makes sense)
-2. Execute database.sql on your database. This will create the tables required by Anatomy2.0
--- src/views/showcase.php
+++ src/views/showcase.php
@@ -24,20 +24,20 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge' charset='utf8'/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Collaborative 3D Model Viewer</title>
-    
+
     <link type='text/css' rel='stylesheet' href='../external/x3dom/x3dom.css'/>
     <link rel='stylesheet' type='text/css' href='../css/showcase.css'/>
   </head>
 
   <body>
-    
+
     <!-- Load all Javascript dependencies. Care: menu.php also loads dependencies -->
 
     <?php require ("menu.php"); ?>
-      
+
     	<script src='../external/yjs/y.js'></script>
       <script src='../external/x3dom/x3dom.js'> </script>
-      
+
       <script src='../js/x3d-extensions.js'> </script>
       <script src='../js/showcase-base.js'> </script>
       <script src='../js/showcase-annotations.js'> </script>
@@ -45,35 +45,50 @@
       <script src='../js/showcase-sync.js'> </script>
       <script src="../js/showcase-highlighting.js"></script>
       <script src="../js/showcase-specials.js"></script>
-    <?php
+      <script src='../js/showcase-help.js'> </script>
 
+    <?php
       include("toolbar.php");
-      
+
       include '../php/db_connect.php';
       $model_id = filter_input(INPUT_GET, "id");
       if (isset($model_id)) {
         $query  = "SELECT * FROM models WHERE id = $model_id";
         $model = $db->query($query)->fetchObject();
       }
-      
-      if (filter_input(INPUT_GET, "widget") == "true") { 
-        $viewer_class = "viewer_object_role"; 
+
+      if (filter_input(INPUT_GET, "widget") == "true") {
+        $viewer_class = "viewer_object_role";
         $initModelViewer = "onload=\"showcase.onModelLoaded()\"";
-      } else { 
-        $viewer_class = "viewer_object"; 
+      } else {
+        $viewer_class = "viewer_object";
         $initModelViewer = "onload=\"showcase.onModelLoaded()\"";
       }
     ?>
-   
+
     <div id="x3d-container"></div>
       <!-- loading first model -->
     <script>
-        showcase.setNewModel(<?php echo $model_id;?>)  
+        showcase.setNewModel(<?php echo $model_id;?>)
     </script>
-      
+
     <!-- Creates a panel with information about mouse usage and hotkeys for navigation -->
-    <?php include("nav_info.html"); ?>
-    <!-- A box for showing annotation content. There will always be just one box at a time. This div will be moved to the correct position. -->
+    <!-- Navigation info: Explanations of mouse usage/hotkeys to navigate the current view will be shown here -->
+    <div id="mainNav" class="navigation-overlay">
+      <div id="buttonArea">
+        <table id="mouseTable">
+        <tr id="mouseRow"><td><img class="navigation-info-img" src="../images/Mouse.gif"></td><td id="mouse"></td></tr>
+        <tr id="mouseLeftRow"><td><img class="navigation-info-img" src="../images/Mouse_Left.gif"></td><td id="mouseLeft"></td></tr>
+        <tr id="mouseRightRow"><td><img class="navigation-info-img" src="../images/Mouse_Right.gif"></td><td id="mouseRight"></td></tr>
+        <tr id="mouseWheelScrollRow"><td><img class="navigation-info-img" src="../images/Mouse_Wheel_Scroll.gif"></td><td id="mouseWheelScroll"></td></tr>
+        </table>
+
+        <table id="buttonTable">
+        </table>
+      </div>
+    </div>
+
+        <!-- A box for showing annotation content. There will always be just one box at a time. This div will be moved to the correct position. -->
     <div id="annotation-content" class="annotation-content hidden">
       <div id="div-annotation-content-loading" class="hidden">
         <h4>Loading...</h4>
